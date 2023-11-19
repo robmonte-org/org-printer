800668db952bcc0add81b96dfdc3fdcc573b3a7b
robmonte <17119716+robmonte@users.noreply.github.com>
2023-11-18 21:54:47 -0600

    push

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/robmonte/org-private-printer
+
+go 1.21.4
---
+++ privateprinter/privateprinter.go
@@ -0,0 +1,10 @@
+package privateprinter
+
+import "fmt"
+
+func PrivatePrint(s string) {
+	fmt.Println()
+	fmt.Println(s)
+	fmt.Println()
+	fmt.Print()
+}
